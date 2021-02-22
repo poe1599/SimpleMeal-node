14b9e67b00e5de9ca8a469e6af8c247c9e2ccafc
poe <poe1599@gmail.com>
2021-02-22 21:07:51 +0800

    0222 優惠券取得路由

--- src/routes/milestone.js
+++ src/routes/milestone.js
@@ -29,7 +29,6 @@ router.use((req, res, next) => {
 // 用query string拿資料 取得成就列表
 // http://localhost:4000/milestone/getMilestoneList?sid=1&filter=finish&page=1&perpage=4
 router.get("/getMilestoneList", async (req, res) => {
-
   let filter = req.query.filter;
   let filterQuery = " ";
   let limit = " limit ";
@@ -44,16 +43,21 @@ router.get("/getMilestoneList", async (req, res) => {
       filterQuery = " and AddProgress < progress_goal or AddProgress is null ";
       break;
     default:
-      filterQuery = " and 1 = 1 "
+      filterQuery = " and 1 = 1 ";
   }
 
-  if(req.query.perpage == 0)//畫面小於576px 不下limit
+  if (req.query.perpage == 0)
+    //畫面小於576px 不下limit
     limit = " ";
   else
-    limit += req.query.perpage*(req.query.page-1)+","+ req.query.perpage+" ";
-  const result = await db.query("select * from (select m.milestone_sid, m.stone_name, m.progress_goal, m.reward_point, m.subs, m.event_startime, m.event_endtime, m.unfinished_goal_pic, m.finished_goal_pic, sum(e.add_progress) AddProgress, t.Subs TriggerSubs from milestone_manager m join trigger_describe t on t.trigger_ID = m.event_trigger left join event_record e on e.event_time > m.event_startime and (m.event_endtime> e.event_time or m.event_endtime is null) and e.member_number = ? and m.event_trigger = e.event_trigger GROUP by m.Milestone_sid) temp where 1=1 "+ filterQuery+ limit, [
-    req.query.sid,
-  ]);
+    limit +=
+      req.query.perpage * (req.query.page - 1) + "," + req.query.perpage + " ";
+  const result = await db.query(
+    "select * from (select m.milestone_sid, m.stone_name, m.progress_goal, m.reward_point, m.subs, m.event_startime, m.event_endtime, m.unfinished_goal_pic, m.finished_goal_pic, sum(e.add_progress) AddProgress, t.Subs TriggerSubs from milestone_manager m join trigger_describe t on t.trigger_ID = m.event_trigger left join event_record e on e.event_time > m.event_startime and (m.event_endtime> e.event_time or m.event_endtime is null) and e.member_number = ? and m.event_trigger = e.event_trigger GROUP by m.Milestone_sid) temp where 1=1 " +
+      filterQuery +
+      limit,
+    [req.query.sid]
+  );
   res.json(result[0]);
 });
 
@@ -61,16 +65,20 @@ router.get("/getMilestoneList", async (req, res) => {
 // http://localhost:4000/milestone/getPoint?sid=1
 //動態產生點數沒有存放在特定位置 每次都要計算 總共取得的點數 以及總共花費的點數 相減而成
 router.get("/getPoint", async (req, res) => {
-
   //將完成的成就點數加總
-  const totalGetPoiont = await db.query("select sum(reward_point) Sum from (select m.milestone_sid, m.stone_name, m.progress_goal, m.reward_point, sum(e.add_progress) AddProgress from milestone_manager m left join event_record e on e.event_time > m.event_startime and (m.event_endtime> e.event_time or m.event_endtime is null) and e.member_number = ? and m.event_trigger = e.event_trigger GROUP by m.milestone_sid) temp where temp.progress_goal <= temp.AddProgress", [
-    req.query.sid,
-  ]);
+  const totalGetPoiont = await db.query(
+    "select sum(reward_point) Sum from (select m.milestone_sid, m.stone_name, m.progress_goal, m.reward_point, sum(e.add_progress) AddProgress from milestone_manager m left join event_record e on e.event_time > m.event_startime and (m.event_endtime> e.event_time or m.event_endtime is null) and e.member_number = ? and m.event_trigger = e.event_trigger GROUP by m.milestone_sid) temp where temp.progress_goal <= temp.AddProgress",
+    [req.query.sid]
+  );
   //將所有花費過的點數加總
-  const totalSpendPoint = await db.query("select sum(spend_point) Sum from `milestone_user` where memner_number = ? ", [
-    req.query.sid,
-  ]);
-  res.json({totalGetPoiont:totalGetPoiont[0][0].Sum,totalSpendPoint:totalSpendPoint[0][0].Sum});
+  const totalSpendPoint = await db.query(
+    "select sum(spend_point) Sum from `milestone_user` where memner_number = ? ",
+    [req.query.sid]
+  );
+  res.json({
+    totalGetPoiont: totalGetPoiont[0][0].Sum,
+    totalSpendPoint: totalSpendPoint[0][0].Sum,
+  });
 });
 
 // // 用params拿資料
@@ -90,6 +98,17 @@ router.get("/getPoint", async (req, res) => {
 //   ]);
 //   res.json(result);
 // });
+
+// 用在購物車取得該會員有什麼優惠券
+router.get("/cartForDiscount", async (req, res) => {
+  const memner_number = req.session.admin.id;
+  const result = await db.query(
+    "SELECT * FROM `milestone_user` WHERE `memner_number` = ? and `good_type` = 2 and `used_date` is null ",
+    [memner_number]
+  );
+
+  res.json(result[0]);
+});
 
 router.use((req, res) => {
   res.type("text/plain");
