e0ff981c917829cd0acd40ddbb43c4c4cc89ccb4
Lydia Liu <tin20630@gmail.com>
2021-02-26 21:07:54 +0800

    修改評論發送

--- src/routes/surpriseKitchenHistory.js
+++ src/routes/surpriseKitchenHistory.js
@@ -18,52 +18,31 @@ router.use((req, res, next) => {
   next();
 });
 
-// 拿訂單資料
+// 送廚房評論
 // http://localhost:4000/surpriseKitchenHistory/getoverlist
-router.get("/getoverlist", async (req, res) => {
+router.post("/getoverlist", async (req, res) => {
     const member_number = req.session.admin.member_number;
     const [
-      row,
-    ] = await db.query(
-      "SELECT * FROM `surprisekitchen_order` WHERE `member_number` = ? and `check_date` is null ",
-      [member_number]
-    );
-    // console.log(row);
-  
-    // 當資料庫沒有預約資料時
-    if (row.length === 0) {
-      res.json([{ sid: 0 }]);
-      return;
-    }
-  
-    // 當資料庫有1筆預約資料時
-    if (row.length === 1) {
-      // 處理日期格式
-      row[0].reservation_date = moment(row[0].reservation_date).format(
-        "YYYY-MM-DD"
+        row,
+      ] = await db.query(
+        "INSERT INTO `surprise_comment`(`sid`, `member_number`, `nickname`, `used_date`, `comment`, `builded_date`) VALUES (null, ?, ?, ?, ?, NOW())",
+        [
+          member_number,
+          req.body.nickname,
+          req.body.used_date,
+          req.body.comment,
+          req.body.builded_date,
+        ]
       );
-      res.json(row);
-      return;
-    }
-  
-    // 當資料庫有多筆預約資料時 (不應該出現此情況, 由當初寫入訂單時控制1個會員只會有1筆未結帳預約)
-    if (row.length >= 1) {
-      res.json([
-        {
-          sid: 0,
-          msg: "不應該出現此情況, 由當初寫入訂單時控制1個會員只會有1筆未結帳預約",
-        },
-      ]);
-      return;
-    }
-  });
+      res.json({ row, msg: "" });
+    });
 
-//單純拿全部
-// http://localhost:4000/surprisekitchenOrder/getReservationInfo
-// router.get("/getReservationInfo", async (req, res) => {
-//   const [result] = await db.query("SELECT `reservation_date`, `reservation_time`FROM `surprisekitchen_order` WHERE 1");
-//   res.json(result);
-// });
+//單純拿驚喜廚房訂單全部
+// http://localhost:4000/surpriseKitchenHistory/getReservationInfo
+router.get("/getReservationInfo", async (req, res) => {
+  const [result] = await db.query("SELECT * FROM `surprisekitchen_order` WHERE 1");
+  res.json(result);
+});
 
 // // 用query string拿資料
 // // http://localhost:4000/test/getmealbyquery?sid=1
